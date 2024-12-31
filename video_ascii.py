10f4b37bbe20e3720270739a914d728cafa8a346
leonardo-toffalini <leonardotoffalini@gmail.com>
2024-12-31 20:27:42 +0100

    remove for loops in rendering

--- video_ascii.py
+++ video_ascii.py
@@ -16,8 +16,10 @@ warnings.filterwarnings("ignore")
 class Args:
     video_path: str
     """The path to the video"""
-    width: int
+    width: int = 60
     """Target width"""
+    fps: int = 30
+    """Target frames per second"""
 
 
 luminosity_table_simplified = " .:-=+*#%@"
@@ -55,59 +57,71 @@ def get_ascii_edge_from_angle(angle):
         return "-"
 
 
+def get_char(x, luma_table):
+    n = len(luma_table)
+    idx = int((n - 1) * x)
+    return luma_table[idx]
+
+
+get_char_vectorized = np.vectorize(get_char)
+get_ascii_edge_from_angle_vectorized = np.vectorize(get_ascii_edge_from_angle)
+
+
 def convert_and_print_ascii(im, args, edge_min):
     im = rgb2gray(im)
+    filtered = filters.difference_of_gaussians(im, low_sigma=1, high_sigma=2)
     sobel_x = filters.sobel_h(im)
     sobel_y = filters.sobel_v(im)
     im = resize_image(im, args.width)
     sobel_x = resize_image(sobel_x, args.width)
     sobel_y = resize_image(sobel_y, args.width)
     luma_table = luminosity_table_simplified
-    # filtered = bw_im  # filters.difference_of_gaussians(bw_im, low_sigma=1, high_sigma=2)
 
     edge_magnitude = np.sqrt(sobel_x**2 + sobel_y**2)
     edge_direction = np.degrees(np.arctan2(sobel_y, sobel_x))
 
-    rows, cols = im.shape[:2]
-    for i in range(rows):
-        for j in range(cols):
-            n = len(luma_table)
-            idx = int((n - 1) * im[i, j])
-            char = luma_table[idx]
-            if edge_magnitude[i, j] > edge_min:
-                char = get_ascii_edge_from_angle(edge_direction[i, j])
-            print(char, end="")
-        print()
+    char_arr = get_char_vectorized(im, luma_table)
+    edge_arr = get_ascii_edge_from_angle_vectorized(edge_direction)
+    temp_arr = np.where(edge_magnitude > edge_min, edge_arr, np.nan)
+    char_arr = np.where(temp_arr == "nan", char_arr, temp_arr)
+
+    for row in char_arr:
+        print("".join(row))
 
 
 def main():
     args = tyro.cli(Args)
     edge_min = 0.1
+    FPS = args.fps
+    FRAMETIME = 1 / FPS
 
     # Path to the .mp4 video
-    video_path = "bad_apple.mp4"
-
-    # Open the video using OpenCV
+    video_path = args.video_path
     cap = cv2.VideoCapture(video_path)
 
-    # Check if the video was opened successfully
     if not cap.isOpened():
-        print("Error: Cannot open video.")
+        print(f"Error: Cannot open video at {args.video_path}.")
     else:
-        # Read and display frames
         while True:
+            frame_start = time.time()
             ret, frame = cap.read()
-            if not ret:  # Exit the loop when the video ends
+            if not ret:
                 print("End of video.")
                 break
 
-            # Convert the frame from BGR (OpenCV default) to RGB (for Matplotlib)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = resize(frame, (frame.shape[0], 2 * frame.shape[1]), anti_aliasing=True)
+            frame = resize(
+                frame, (frame.shape[0], 2 * frame.shape[1]), anti_aliasing=False
+            )
             convert_and_print_ascii(frame, args, edge_min)
-            
+            frame_end = time.time()
 
-    # Release the video capture object
+            # wait if the rendering took less than 1/FPS seconds
+            render_time = frame_end - frame_start
+            diff = FRAMETIME - render_time
+            if diff > 0:
+                time.sleep(diff)
+
     cap.release()
 
 
